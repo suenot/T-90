5fa050a21136eb759ade5769c77fae9cb1eef56b
Eugen Soloviov <suenot@gmail.com>
2016-02-12 21:49:49 +0300

    fix autoprofixer

--- .gulp/default/sass.js
+++ .gulp/default/sass.js
@@ -7,14 +7,18 @@ const gutil = require('gulp-util');
 const gulpif = require('gulp-if');
 const autoprefixerOptions = require('../utils/config').autoprefixer;
 const browserSync = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 gulp.task('sass', function () {
 	gulp.src([
 		'assets/**/**/*.{sass,scss}',
 		'!assets/**/**/_*.{sass,scss}'
 	])
+	.pipe(gulpif(isDevelopment, sourcemaps.init()))
 	.pipe(sass().on('error', sass.logError))
 	.pipe(gulpif(gutil.env.prefix, postcss([autoprefixer(autoprefixerOptions)])))
+	.pipe(gulpif(isDevelopment, sourcemaps.write()))
 	.pipe(gulp.dest('public'))
 	.pipe(browserSync.reload({stream: true}))
 });
--- .gulp/default/stylus.js
+++ .gulp/default/stylus.js
@@ -14,6 +14,8 @@ const changed = require('gulp-changed');
 const gutil = require('gulp-util');
 const gulpif = require('gulp-if');
 const autoprefixerOptions = require('../utils/config').autoprefixer;
+const sourcemaps = require('gulp-sourcemaps');
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 src.styl = {
 	'files': [
@@ -25,10 +27,12 @@ src.styl = {
 
 gulp.task('styl', function() {
 	return gulp.src(src.styl.files)
+	.pipe(gulpif(isDevelopment, sourcemaps.init()))
 	// .pipe(changed(src.styl.dest, {extension: '.css'}))
 	.pipe(plumber({errorHandler: onError}))
 	.pipe(stylus())
 	.pipe(gulpif(gutil.env.prefix, postcss([autoprefixer(autoprefixerOptions)])))
+	.pipe(gulpif(isDevelopment, sourcemaps.write()))
 	.pipe(gulp.dest(src.styl.dest))
 	.pipe(browserSync.reload({stream: true}))
 });
