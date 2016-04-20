50f7124e30113fd705a8ac0c1a96386640187ea3
Eugen Soloviov <suenot@gmail.com>
2016-04-20 12:35:11 +0300

    + build

--- .gulp/default/watch.js
+++ .gulp/default/watch.js
@@ -1,11 +1,15 @@
 'use strict';
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var build = gutil.env.build;
 
 // gulp.task('watch', ['setWatch'], function() {
 gulp.task('watch', function() {
-	gulp.watch('assets/**/**/**/*.jade', ['jade', 'minHtml']);
-	gulp.watch('assets/**/**/**/*.html', ['nunjucks', 'minHtml']);
-	gulp.watch('assets/{css,blocks}/**/**/*.styl', ['styl', 'minCss']);
-	gulp.watch('assets/{css,blocks}/**/**/*.{sass,scss}', ['sass', 'minCss']);
-	gulp.watch(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.gif', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css'], ['copy', 'minJs']);
+	if (!build) {
+		gulp.watch('assets/**/**/**/*.jade', ['jade', 'minHtml']);
+		gulp.watch('assets/**/**/**/*.html', ['nunjucks', 'minHtml']);
+		gulp.watch('assets/{css,blocks}/**/**/*.styl', ['styl', 'minCss']);
+		gulp.watch('assets/{css,blocks}/**/**/*.{sass,scss}', ['sass', 'minCss']);
+		gulp.watch(['assets/**/*.png', 'assets/**/*.jpg', 'assets/**/*.gif', 'assets/**/*.ico', 'assets/**/*.txt', 'assets/**/*.xml', 'assets/**/*.eot', 'assets/**/*.svg', 'assets/**/*.ttf', 'assets/**/*.woff', 'assets/**/*.woff2', 'assets/**/*.otf', 'assets/**/*.js', 'assets/**/*.css'], ['copy', 'minJs']);
+	}
 });
--- README.md
+++ README.md
@@ -14,8 +14,17 @@
 ```--open``` - open site in browser
 ```--live``` - livereload turnOn
 ```--prefix``` - css prefixs for old browsers, autostart in production
+```--html``` - optimize and minify html in *public*
+```--critical``` - [critical css](https://github.com/addyosmani/critical) optimize
+```--css``` - optimize, concat and minify css in *public*
+```--js``` - optimize, concat and minify js in *public*
+```--img``` - optimize images in *public*
 ```--build``` - without server and watch
 
+```gulp minImgAssets``` - optimize images in *assets*
+```gulp deploy``` - commit and push *public* to gh-pages
 
 ### For production: without sourcemaps in css and with clean *public* folder:
 ```NODE_ENV=production gulp```
+### Advanced production:
+```NODE_ENV=production gulp --html --js --img --critical --css```
